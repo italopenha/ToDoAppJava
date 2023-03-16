d6811a23656fb6f20298c3da71281954db7cb467
Ítalo Penha <italopenha77@outlook.com>
2023-03-16 20:15:03 -0300

    Iniciando conexão com banco de dados

---
+++ app/src/main/java/util/ConnectionFactory.java
@@ -0,0 +1,12 @@
+package util;
+
+/**
+ *
+ * @author italo
+ */
+public class ConnectionFactory {
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/todoapp";
+    public static final String USER = "root";
+    public static final String PASS = "";
+}
