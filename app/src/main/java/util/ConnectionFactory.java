0cfa04e206d6d6ec66c8ac4832a57759a4af4f87
Ítalo Penha <italopenha77@outlook.com>
2023-03-16 20:52:00 -0300

    Conexão com banco de dados finalizada

--- app/src/main/java/util/ConnectionFactory.java
+++ app/src/main/java/util/ConnectionFactory.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author italo
@@ -9,4 +12,23 @@ public class ConnectionFactory {
     public static final String URL = "jdbc:mysql://localhost:3306/todoapp";
     public static final String USER = "root";
     public static final String PASS = "";
+    
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro na conexão com o banco de dados!", ex);
+        }
+    }
+    
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro na conexão com o banco de dados!", ex);
+        }
+    }
 }
