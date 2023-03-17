1a8f6572978fb4c730289b9ab33df5d6eb70f846
Ítalo Penha <italopenha77@outlook.com>
2023-03-17 17:17:30 -0300

    [Correções em TaskController e ConnectionFactory] {17/03/2023 17:17}

--- app/src/main/java/controller/TaskController.java
+++ app/src/main/java/controller/TaskController.java
@@ -35,7 +35,7 @@ public class TaskController {
         } catch (SQLException ex) {
             throw new RuntimeException("Erro ao salvar a tarefa " + ex.getMessage(), ex);
         } finally {
-            ConnectionFactory.closeConnection(connection);
+            ConnectionFactory.closeConnection(connection, statement);
         }
     }
     
@@ -46,18 +46,18 @@ public class TaskController {
     public void removeById(int taskId) throws SQLException {
         String sql = "DELETE FROM tasks WHERE id = ?";
         
-        Connection conn = null;
+        Connection connection = null;
         PreparedStatement statement = null;
         
         try {
-            conn = ConnectionFactory.getConnection();
-            statement = conn.prepareStatement(sql);
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
             statement.setInt(1, taskId);
             statement.execute();
-        } catch (SQLException ex) {
-            throw new SQLException("Erro ao deletar a tarefa!");
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao deletar a tarefa!");
         } finally {
-            ConnectionFactory.closeConnection(conn);
+            ConnectionFactory.closeConnection(connection, statement);
         }
     }
     
