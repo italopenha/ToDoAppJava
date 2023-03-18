90caaca5ed16aa8d52a15421371173981c89099e
Ítalo Penha <italopenha77@outlook.com>
2023-03-18 20:54:08 -0300

    [Criado novo método closeConnection em ConnectionFactory {18/03/2023 20:53}]

--- app/src/main/java/util/ConnectionFactory.java
+++ app/src/main/java/util/ConnectionFactory.java
@@ -3,6 +3,7 @@ package util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  *
@@ -41,6 +42,24 @@ public class ConnectionFactory {
             
             if (statement != null) {
                 statement.close();
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro na conexão com o banco de dados!", ex);
+        }
+    }
+    
+    public static void closeConnection(Connection connection, PreparedStatement statement, ResultSet resultSet) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+            
+            if (statement != null) {
+                statement.close();
+            }
+            
+            if (resultSet != null) {
+                resultSet.close();
             }
         } catch (Exception ex) {
             throw new RuntimeException("Erro na conexão com o banco de dados!", ex);
